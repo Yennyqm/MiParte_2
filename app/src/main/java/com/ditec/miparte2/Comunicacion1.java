31dd177564c196ff28a726814b1b822a1219d9fc
Yennyqm <yennyjulissa@gmail.com>
2021-02-27 20:44:40 -0500

    Iniciando una actividad esperando datos de ella caso 2, validación para casilleros vacios y saltos de linea con etiqueta xml

--- app/src/main/java/com/ditec/miparte2/Comunicacion1.java
+++ app/src/main/java/com/ditec/miparte2/Comunicacion1.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,18 +17,33 @@ public  class  Comunicacion1	extends AppCompatActivity { EditText  edtnombre;
     }
 
     public  void  Verificar(View view)  {
-        Intent  intent  =  new Intent(this,  Comunicacion2.class); intent.putExtra("nombre",  edtnombre.getText().toString());
+        if (edtnombre.getText().length()!=0 && edtingresomes.getText().length()!=0) {
+        Intent  intent  =  new Intent(this,  Comunicacion2.class);
+        intent.putExtra("nombre",  edtnombre.getText().toString());
         intent.putExtra("ingresomes",  Integer.parseInt(edtingresomes.getText().toString()));
 // startActivity(intent);
         startActivityForResult(intent, 1234);
+    }
+        else
+            Toast.makeText(this,"Debe ingresar un nombre y un valor",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected  void  onActivityResult(int  requestCode,
                                       int  resultCode,  Intent  data)  { super.onActivityResult(requestCode,  resultCode,  data); TextView miresultado  =  findViewById(R.id.txtvalor);
-        if  (requestCode  ==  1234  &&  resultCode  ==  RESULT_OK)  { String  res  =  data.getExtras().getString("resultado"); if  (res.equals("aceptado"))
-            miresultado.setText("Esperamos  que  trabajar  con  nosotros  sea  de  su  agrado"); else
+        if  (requestCode  ==  1234  &&  resultCode  ==  RESULT_OK)  { String  res  =  data.getExtras().getString("resultado");
+             if  (res.equals("aceptado"))
+            miresultado.setText("Esperamos  que  trabajar  con  nosotros  sea  de  su  agrado");
+             else
             miresultado.setText("Esperemos  que  cambie  de  opinion  para  trabajar  con  nosotros");
+           }
+        if  (requestCode  ==  3500  &&  resultCode  ==  RESULT_OK)  {
+            miresultado.setText("Visitenos  en  alguna  de  nuestras  sucursales");
         }
+
+    }
+
+    public void sucursales(View view) {
+        startActivityForResult(new  Intent(this,  Comunicacion3.class),  3500);
     }
 }
